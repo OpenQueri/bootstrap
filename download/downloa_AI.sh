3a38ca12e3aab060ef1759fbb481ba5fa4157743
Kiril <csgocvadimkiril80@gmail.com>
2026-05-25 15:42:04 +0200

    Update

--- download/downloa_AI.sh
+++ download/downloa_AI.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 cd ..
 
-wget https://huggingface.co/intfloat/multilingual-e5-base/resolve/main/onnx/model.onnx
-wget https://huggingface.co/intfloat/multilingual-e5-base/resolve/main/onnx/tokenizer.json
+if [ ! -f "model.onnx" ]; then
+  echo "Downloading model.onnx..."
+  wget https://huggingface.co/intfloat/multilingual-e5-base/resolve/main/onnx/model.onnx
+else
+  echo "model.onnx already exists, skipping"
+fi
+
+if [ ! -f "tokenizer.json" ]; then
+  echo "Downloading tokenizer.json..."
+  wget https://huggingface.co/intfloat/multilingual-e5-base/resolve/main/onnx/tokenizer.json
+else
+  echo "tokenizer.json already exists, skipping"
+fi
--- start_main.sh
+++ start_main.sh
@@ -13,7 +13,13 @@ make -j$(nproc) download-backend
 
 make -j$(nproc) download-frontend
 
-docker-compose up -d
+sudo usermod -aG docker $USER
+
+newgrp docker <<EOF
+docker compose up -d
+EOF
+
+docker compose up -d
 
 make -j$(nproc) compil-frontend
 
