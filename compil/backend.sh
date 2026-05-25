0cde92e6475e8213241b4b0ff17ea4147910f4db
Kiril <csgocvadimkiril80@gmail.com>
2026-05-25 16:05:10 +0200

    Update

--- compil/backend.sh
+++ compil/backend.sh
@@ -10,6 +10,14 @@ sudo apt install -y build-essential
 cd OpenQueri-backend/
 echo "DATABASE_URL=postgres://user:password@127.0.0.1:5432/open_queri" > .env
 
+cargo install sqlx-cli --no-default-features --features postgres
+
+unset SQLX_OFFLINE
+
+cargo sqlx prepare
+
+export SQLX_OFFLINE=true
+
 cargo build --release
 
 mv target/release/OpenQueri-backend ../../..
